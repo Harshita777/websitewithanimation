ee0bc6c76ac91443a1b5778c9884b4f00a9532cd
Harshita Kumari <harshita.kumari@techenhance.com>
2024-02-02 00:38:45 +0530

    navbar completed í¸

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,30 @@
-import "../navbar.css"
-import logo from "../../public/logo.png"
-
+import "../navbar.css";
+import logo from "../../public/logo.png";
+import { TfiEmail } from "react-icons/tfi";
 const Navbar = () => {
   return (
-   <div className="navbar">
-    <div className="navbar-logo">
-    <img src={logo} alt="logo" />
+    <div className="flex justify-between container mx-auto max-w-5xl">
+      <div className="h-20 w-20 navbar-logo p-2">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="">
+        <ul className="flex list-none">
+          <li className="p-4">WORK</li>
+          <li className="p-4">EXPERTISE</li>
+          <li className="p-4">ABOUT</li>
+          <li className="p-4">INSIGHT</li>
+          <li className="p-2">
+            <div className="flex justify-center gap-2 items-center bg-yellow-400 border-black rounded text-black h-10 w-40">
+              <div className="">
+                <TfiEmail className="" />
+              </div>
+              <div>START A PROJECT</div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
-     <div className="navbar-list">
-     <ul className="flex" >
-        <li>WORK</li>
-        <li>EXPERTISE</li>
-        <li>ABOUT</li>
-        <li>INSIGHT</li>
-        <li>
-            <button>START A PROJECT</button>
-        </li>
-     </ul>
-     </div>
-     
-   </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
